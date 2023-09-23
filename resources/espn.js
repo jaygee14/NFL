bbfc0167556004ea4ec773d91189fd31030b4827
jaygee14 <Jared3257@gmail.com>
2023-09-23 01:20:14 -0400

    Parse Data and Generate Scoreboard

--- resources/espn.js
+++ resources/espn.js
@@ -6,14 +6,69 @@ function getGames() {
     })
 }
 
-function getImg(data) {
-    let img_links = []
+function getData(data) {
+    let img_links = [];
+    let scores = []
 
     for (game of data) {
-        img_links.push(game.competitions[0].competitors[1].team.logo)
-        img_links.push(game.competitions[0].competitors[0].team.logo)
+        let images = []
+        let games = []
+        images.push(game.competitions[0].competitors[1].team.logo)
+        images.push(game.competitions[0].competitors[0].team.logo)
+
+        games.push(game.competitions[0].competitors[1].score)
+        games.push(game.competitions[0].competitors[0].score)
+
+        scores.push(games)
+        img_links.push(images)
     }
-    return [img_links]
+    const answer = { images: [img_links], scores: [scores] }
+    console.log(answer.images)
+    return answer
 }
 
-getGames().then(getImg)
+function generateScoreboard(object) {
+    const scoreboard = document.getElementById('scoreBoard');
+    const div = document.createElement('div');
+    div.classList.add('game');
+
+    scoreboard.appendChild(div)
+
+    i = 0;
+
+    for (img of object.images){
+        const game = document.getElementsByClassName('game')
+        const div = document.createElement('div')
+        div.classList.add('game')
+        //create elements
+        const awayImg = document.createElement('img')
+        const awayScore = document.createElement('p')
+        //add assets
+        awayImg.src = img[i][0];
+        awayScore.textContent = object.scores[i][0][0]
+        const tag = document.createElement('p')
+        tag.textContent = 'vs';
+        tag.classList.add('tag')
+        //add to div
+        game[i].appendChild(awayImg);
+        game[i].appendChild(awayScore);
+        game[i].appendChild(tag);
+
+        const homeImg = document.createElement('img')
+        const homeScore = document.createElement('p')
+        //add assets
+        homeImg.src = img[i][1];
+        homeScore.textContent = object.scores[i][0][1]
+        //add to div
+        game[i].appendChild(homeImg);
+        game[i].appendChild(homeScore);
+
+        scoreboard.appendChild(div)
+        i++
+    }
+}
+
+function printData(data) {
+    console.log(data[0].competitions[0].competitors[0].score)
+}
+getGames().then(getData).then(generateScoreboard)
